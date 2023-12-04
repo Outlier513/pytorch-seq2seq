d8344cb851bed2aa0c61342aca5bd0ea08dbce58
Outlier <likehaoxxx@gmail.com>
2023-12-04 15:48:36 +0800

    commit mask

--- dataloader.py
+++ dataloader.py
@@ -5,12 +5,14 @@ from torchtext.vocab import build_vocab_from_iterator
 from torchtext.data import get_tokenizer, to_map_style_dataset
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
+from functools import partial
 
 
 def yiled_token(dataset, tokenizer):
-    index = 0 if type(tokenizer.keywords['spacy']) == spacy.lang.de.German else 1
+    index = 0 if type(tokenizer.keywords["spacy"]) == spacy.lang.de.German else 1
     for items in dataset:
         yield tokenizer(items[index].lower())
+
 
 def transform2token(dataset, tokenizer_src, tokenizer_trg, vocab_src, vocab_trg):
     length = len(dataset)
@@ -18,45 +20,104 @@ def transform2token(dataset, tokenizer_src, tokenizer_trg, vocab_src, vocab_trg)
         src, trg = dataset._data[i]
         src = tokenizer_src(src)
         trg = tokenizer_trg(trg)
-        src = [vocab_src['<bos>']] + [vocab_src[x.lower()] for x in src] + [vocab_src['<eos>']]
-        trg = [vocab_trg['<bos>']] + [vocab_trg[x.lower()] for x in trg] + [vocab_trg['<eos>']]
+        src = (
+            [vocab_src["<bos>"]]
+            + [vocab_src[x.lower()] for x in src]
+            + [vocab_src["<eos>"]]
+        )
+        trg = (
+            [vocab_trg["<bos>"]]
+            + [vocab_trg[x.lower()] for x in trg]
+            + [vocab_trg["<eos>"]]
+        )
         dataset._data[i] = (torch.LongTensor(src), torch.LongTensor(trg))
     return dataset
 
-def collate_batch(batch):
-    src_list, trg_list = [], []
+
+def collate_batch(batch, include_length=False):
+    src_list, trg_list, src_len_list = [], [], []
     for src, trg in batch:
         src_list.append(src)
         trg_list.append(trg)
-    return pad_sequence(src_list), pad_sequence(trg_list)
+        src_len_list.append(len(src))
+    if include_length:
+        return (
+            pad_sequence(src_list),
+            torch.LongTensor(src_len_list),
+            pad_sequence(trg_list),
+        )
+    else:
+        return pad_sequence(src_list), pad_sequence(trg_list)
+
 
 def batch_sample(dataset, batch_size):
     indices = [(i, len(s[0])) for i, s in enumerate(dataset)]
     while True:
         random.shuffle(indices)
         pooled_indices = []
-        for i in range(0, len(indices), batch_size*100):
-            pooled_indices.extend(sorted(indices[i:i + batch_size * 100], key=lambda x: x[1]))
+        for i in range(0, len(indices), batch_size * 100):
+            pooled_indices.extend(
+                sorted(
+                    indices[i : i + batch_size * 100], key=lambda x: x[1], reverse=True
+                )
+            )
         pooled_indices = [x[0] for x in pooled_indices]
         for i in range(0, len(pooled_indices), batch_size):
-            yield pooled_indices[i:i + batch_size]
+            yield pooled_indices[i : i + batch_size]
 
 
 def get_tokenizer_and_vocab(dataset):
-    tokenizer_de, tokenizer_en = get_tokenizer('spacy','de_core_news_sm'), get_tokenizer('spacy','en_core_web_sm')
-    vocab_de = build_vocab_from_iterator(yiled_token(dataset, tokenizer_de), min_freq=2, specials=['<pad>','<unk>','<bos>','<eos>'])
-    vocab_en = build_vocab_from_iterator(yiled_token(dataset, tokenizer_en), min_freq=2, specials=['<pad>','<unk>','<bos>','<eos>'])
+    tokenizer_de, tokenizer_en = get_tokenizer(
+        "spacy", "de_core_news_sm"
+    ), get_tokenizer("spacy", "en_core_web_sm")
+    vocab_de = build_vocab_from_iterator(
+        yiled_token(dataset, tokenizer_de),
+        min_freq=2,
+        specials=["<pad>", "<unk>", "<bos>", "<eos>"],
+    )
+    vocab_en = build_vocab_from_iterator(
+        yiled_token(dataset, tokenizer_en),
+        min_freq=2,
+        specials=["<pad>", "<unk>", "<bos>", "<eos>"],
+    )
     vocab_de.set_default_index(1)
     vocab_en.set_default_index(1)
     return tokenizer_de, tokenizer_en, vocab_de, vocab_en
 
-def get_dataloader_and_etc(train_dataset, val_dataset, test_dataset, batch_size = 8):
-    train_dataset, val_dataset, test_dataset = to_map_style_dataset(train_dataset),to_map_style_dataset(val_dataset), to_map_style_dataset(test_dataset)
-    tokenizer_de, tokenizer_en, vocab_de, vocab_en = get_tokenizer_and_vocab(train_dataset)
-    train_dataset = transform2token(train_dataset, tokenizer_de, tokenizer_en, vocab_de, vocab_en)
-    val_dataset = transform2token(val_dataset, tokenizer_de, tokenizer_en, vocab_de, vocab_en)
-    test_dataset = transform2token(test_dataset, tokenizer_de, tokenizer_en, vocab_de, vocab_en)
-    train_dataloader = DataLoader(train_dataset, batch_sampler=batch_sample(train_dataset, batch_size), collate_fn=collate_batch)
-    val_dataloader = DataLoader(test_dataset, batch_sampler=batch_sample(test_dataset, batch_size), collate_fn=collate_batch)
-    test_dataloader = DataLoader(val_dataset, batch_sampler=batch_sample(val_dataset, batch_size), collate_fn=collate_batch)
-    return train_dataloader, val_dataloader, test_dataloader,(tokenizer_de, tokenizer_en, vocab_de, vocab_en)
+
+def get_dataloader_and_etc(
+    train_dataset, val_dataset, test_dataset, batch_size=8, include_length=False
+):
+    tokenizer_de, tokenizer_en, vocab_de, vocab_en = get_tokenizer_and_vocab(
+        train_dataset
+    )
+    train_dataset = transform2token(
+        train_dataset, tokenizer_de, tokenizer_en, vocab_de, vocab_en
+    )
+    val_dataset = transform2token(
+        val_dataset, tokenizer_de, tokenizer_en, vocab_de, vocab_en
+    )
+    test_dataset = transform2token(
+        test_dataset, tokenizer_de, tokenizer_en, vocab_de, vocab_en
+    )
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_sampler=batch_sample(train_dataset, batch_size),
+        collate_fn=partial(collate_batch, include_length=include_length),
+    )
+    val_dataloader = DataLoader(
+        test_dataset,
+        batch_sampler=batch_sample(test_dataset, batch_size),
+        collate_fn=partial(collate_batch, include_length=include_length),
+    )
+    test_dataloader = DataLoader(
+        val_dataset,
+        batch_sampler=batch_sample(val_dataset, batch_size),
+        collate_fn=partial(collate_batch, include_length=include_length),
+    )
+    return (
+        train_dataloader,
+        val_dataloader,
+        test_dataloader,
+        (tokenizer_de, tokenizer_en, vocab_de, vocab_en),
+    )
--- utils.py
+++ utils.py
@@ -2,31 +2,46 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 
+
 def init_weights1(m):
     for name, param in m.named_parameters():
         nn.init.uniform_(param.data, -0.08, 0.08)
+
 
 def init_weights2(m):
     for name, param in m.named_parameters():
         nn.init.normal_(param.data, mean=0, std=0.01)
 
+
 def init_weights3(m):
     for name, param in m.named_parameters():
         nn.init.normal_(param.data, mean=0, std=0.01)
 
+
 def count_parameters(model):
-    print(f'The model has {sum(p.numel() for p in model.parameters() if p.requires_grad):,}trainable parameters')
+    print(
+        f"The model has {sum(p.numel() for p in model.parameters() if p.requires_grad):,}trainable parameters"
+    )
 
 
-def train(epoch, model, dataloader, n_batch ,optimizer, criterion, clip, device):
+def train(
+    epoch, model, dataloader, n_batch, optimizer, criterion, clip, device, packed=False
+):
     model.train()
     train_loss = 0
-    with tqdm(desc=f'Epoch:{epoch+1: 2d}',total=n_batch) as pbar:
-        for i, (src, trg) in enumerate(dataloader):
-            src = src.to(device)
-            trg = trg.to(device)
+    with tqdm(desc=f"Epoch:{epoch+1: 2d}", total=n_batch) as pbar:
+        for i, data in enumerate(dataloader):
             optimizer.zero_grad()
-            output = model(src, trg)
+            if packed:
+                src, src_len, trg = data
+                src = src.to(device)
+                trg = trg.to(device)
+                output = model(src, src_len, trg)
+            else:
+                src, trg = data
+                src = src.to(device)
+                trg = trg.to(device)
+                output = model(src, trg)
             # output [trg_len-1, bs, output_dim]
             output_dim = output.shape[-1]
             output = output.view(-1, output_dim)
@@ -36,28 +51,36 @@ def train(epoch, model, dataloader, n_batch ,optimizer, criterion, clip, device)
             torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
             optimizer.step()
             train_loss += loss.item()
-            avg_loss = train_loss/(i+1)
-            pbar.set_postfix({'train_loss':avg_loss})
+            avg_loss = train_loss / (i + 1)
+            pbar.set_postfix({"train_loss": avg_loss})
             pbar.update()
-            if i == n_batch-1:
+            if i == n_batch - 1:
                 break
 
-def evaluate(model, dataloader, n_batch , criterion, device):
+
+def evaluate(model, dataloader, n_batch, criterion, device, packed=False):
     model.eval()
     eval_loss = 0
     with tqdm(total=n_batch) as pbar:
-        for i, (src, trg) in enumerate(dataloader):
-            src = src.to(device)
-            trg = trg.to(device)
-            output = model(src, trg)
+        for i, data in enumerate(dataloader):
+            if packed:
+                src, src_len, trg = data
+                src = src.to(device)
+                trg = trg.to(device)
+                output = model(src, src_len, trg)
+            else:
+                src, trg = data
+                src = src.to(device)
+                trg = trg.to(device)
+                output = model(src, trg)
             output_dim = output.shape[-1]
             output = output.view(-1, output_dim)
             trg = trg[1:].view(-1)
             loss = criterion(output, trg)
             eval_loss += loss.item()
-            avg_loss = eval_loss/(i+1)
-            pbar.set_postfix({'eval_loss':avg_loss})
+            avg_loss = eval_loss / (i + 1)
+            pbar.set_postfix({"eval_loss": avg_loss})
             pbar.update()
-            if i == n_batch-1:
+            if i == n_batch - 1:
                 break
     return avg_loss
